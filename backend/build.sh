03fabb2519e9c4c894bf3b79d4c86ed7b8e5cb7b
Ameyaw Williams Kyere <williams.kyere@stu.ucc.edu.gh>
2026-05-09 09:55:51 +0000

    first deployment fail

--- backend/build.sh
+++ backend/build.sh
@@ -2,5 +2,10 @@
 
 set -o errexit
 
-python manage.py migrate
+# Ensure pip is up-to-date and install dependencies
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run migrations and collect static files
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
