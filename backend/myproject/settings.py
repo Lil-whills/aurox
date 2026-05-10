898df737fed6cfa06d71fd8861d0261741646b31
Ameyaw Williams Kyere <williams.kyere@stu.ucc.edu.gh>
2026-05-10 13:04:02 +0000

    commented again the SECURE_SSL_REDIRECT

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -137,15 +137,11 @@ if _render_hostname:
 
 # Security
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
+ #   SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
 
 
 # Password validation
